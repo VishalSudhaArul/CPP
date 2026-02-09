2751d8979edf7c355725b2cee4d9dcb7facde647
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-09 20:27:42 +0530

    Create 02.cpp

---
+++ 02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    const int dob = 2000;
+
+    int age =24;
+    age = 25;
+
+    cout<<"My age is \n"<<age; 
+    cout<<"I was born in " <<dob;
+    return 0;
+}
