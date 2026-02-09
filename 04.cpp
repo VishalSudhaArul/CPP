6810b551ea0c7491ac31bafaef23195fbf852c73
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-09 22:27:00 +0530

    Arithmetic operators

---
+++ 04.cpp
@@ -0,0 +1,24 @@
+//Operators
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a=5;
+    int b=6;
+
+    int sum = a+b;
+    int dif = a-b;
+
+    float div = float(a) /b; // division operator - integer division
+    // Explicit Type conversion
+    cout<<"sum : "<<sum<<endl;
+    cout<<"dif : "<<dif<<endl;
+    cout<<"div :"<<div<<endl;
+    return 0;
+}
+
+//Artimtic operators 
+// +, -, *, /, %, ++, --
+
