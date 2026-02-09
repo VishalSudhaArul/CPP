af92020167005de6cb96a3418cd90a66018d9ad3
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-09 20:24:56 +0530

    Print Hello World

---
+++ 01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Your Code goes here!
+    cout<<"helloWorld\n";
+    cout<<"I am 20 yrs old";
+    return 0;
+}
+
+/*
+Escape Sequence
+\n - new line character
+\t - new space 
+*/
