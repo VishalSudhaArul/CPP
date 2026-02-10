ae207a13b62cbd8d1d597a59c8dfa2367b98d443
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-10 13:34:38 +0530

    do while

---
+++ 07.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     //Your Code goes here!
+    
+//     int n = 5;  
+//     int i = 6;   
+    
+//     do
+//     {
+//         cout<<i<<endl;  
+//         i++;      
+//     }
+//     while(i<=n);
+
+//     return 0;
+// }
+
+// do while loop
+// while - entry control loop
+// do while - exit control loop
+
+
+int main(){
+
+int n=0;
+int i=5;
+
+do{
+    cout<<i<<endl;
+    i--;
+} while(i>=n);
+
+
+
+    return 0;
+}
