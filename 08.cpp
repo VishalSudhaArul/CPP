c4c5b3fd2bb14717c65d7b076edf795474885b01
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-10 13:35:20 +0530

    while loop 2

---
+++ 08.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Your Code goes here!
+
+    char c;
+
+    do{
+        cout<<"Enter a character = ";
+        cin>>c;
+        cout<<"You Entered:  "<<c<<endl;
+    } while (c!='q');
+
+    return 0;
+}
