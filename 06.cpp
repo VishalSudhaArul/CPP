d0594250cb29c7b3dfc0ce10401f3741b0e7c51b
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-10 13:33:50 +0530

    while loops

---
+++ 06.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Your Code goes here!
+
+    // cout<<" 1 2 3 4 5 6 7 8";
+    int n = 150000;  // boundary - count
+    int i = 0;    // iterator - initialize -1
+    while(i<=n)   // condition check 
+    {
+        cout<<i<<endl;  // print action
+        i++;      // update   -3 
+    }
+
+
+    return 0;
+}
+
+
+//Iterative Statements - loops
+// - while loops
