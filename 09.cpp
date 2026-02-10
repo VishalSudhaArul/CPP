49a812c671d973b28dddba5f3e885d7526a37def
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-10 13:36:00 +0530

    for loop

---
+++ 09.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        int n=0;
+    for(int i=1000; i>=n; i--)
+    {
+        cout<<i<<endl;
+    }
+
+
+    return 0;
+}
+
+/*
+ for loop -definitive count
+ -Init 
+ -condition
+ -update
+*/
