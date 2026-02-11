3e95d2bd3b60ba4beb9699752637aa223ba4649b
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-11 22:17:24 +0530

    math functions

---
+++ 13.cpp
@@ -0,0 +1,23 @@
+// math functions
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n1=5, n2=7;
+
+    cout<< max(n1,n2)<<endl;
+    cout<< min(n1,n2)<<endl;
+
+    return 0;
+}
+
+/*
+Math functions
+-min
+-max
+-sqrt
+-round
+-log
+*/
